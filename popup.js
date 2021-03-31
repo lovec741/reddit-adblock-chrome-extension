30c8c770d43a0f6ca36549504dcacb05dd7af29f
lovec741 <32958076+lovec741@users.noreply.github.com>
2021-03-31 12:17:27 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,15 +1,19 @@
 const am_el = document.getElementById("am")
 function setAdBlockedAmount(am) {
-	am_el.innerHTML = "Ads Blocked: "+am
+	if (am == undefined) {
+		am_el.innerHTML = "You are not on Reddit!"
+	} else {
+		am_el.innerHTML = "Ads Blocked: "+am
+	}
 }
 window.addEventListener('DOMContentLoaded', () => {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, tabs => {
-    chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'popup', subject: 'ad_am'},
-        setAdBlockedAmount)
-  })
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, tabs => {
+	chrome.tabs.sendMessage(
+		tabs[0].id,
+		{from: 'popup', subject: 'ad_am'},
+		setAdBlockedAmount)
+	})
 })
